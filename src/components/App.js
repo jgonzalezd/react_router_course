6c2a0514014fa09303dcddf70ea0ba0f9ade0b07
Julian Gonzalez <juliangonzalez.code@gmail.com>
2017-05-17 13:03:24 -0500

    finished

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Header   from './Header'
+import Home     from './Home'
+import About    from './About'
+import Teachers from './Teachers'
+import Courses  from './Courses'
+import NotFound from './NotFound'
+import Featured from './Featured'
+
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Switch>
+        <Route exact path="/"         component={Home} />
+        <Route       path="/about"    component={About} />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route       path="/teachers/:course/:name/" component={Featured}/>
+        <Route       path="/foo"      component={Courses} />
+        <Route                        component={NotFound} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+
+);
+
+export default App;
---
+++ src/components/Featured.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Featured = ({match}) => {
+
+  let name   = match.params.name;
+  let course = match.params.course;
+
+  return (
+    <div className="main-content">
+      <h2>Featured: </h2>
+      <p>Introducing <strong>{ name }</strong>, a teacher who loves teaching courses about <strong>{course}</strong>!</p>
+    </div>
+  );
+}
+
+export default Featured;
